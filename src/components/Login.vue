cbbc05bd128f9aba3e3d3f064fdc469153b1b7d4
savanitdev <savanitdev@gmail.com>
2019-08-23 17:13:01 +0700

    Initial  commit

---
+++ src/components/Login.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m8 offset-m2">
+          <div class="login card-panel blue white-text center">
+            <h3>Login</h3>
+            <form>
+              User name:
+              <br />
+              <div class="input-field">
+                <i class="material-icons prefix">email</i>
+                <input type="text" name="email" id="email" v-model="email" />
+                <label class="white-text" for="email">email</label>
+              </div>
+              <br />User password:
+              <br />
+              <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input type="password" name="password" id="password" v-model="password" />
+                <label class="white-text" for="email">password</label>
+              </div>
+              <button v-on:click="login" class="btn btn-large lighten-4-black-text">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+export default {
+  name: "login",
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    login: function(e) {
+      //    console.log(this.email);
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
+            alert(`You are logged is as ${user.user.email}`);
+
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    }
+  }
+};
+</script>
---
+++ src/components/Register.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        <div class="col s12 m8 offset-m2">
+          <div class="login card-panel green white-text center">
+            <h3>Register</h3>
+            <form>
+              User name:
+              <br />
+              <div class="input-field">
+                <i class="material-icons prefix">email</i>
+                <input type="text" id="email" name="email" v-model="email" />
+                <label class="white-text" for="email">email</label>
+              </div>
+              <br />User password:
+              <br />
+              <div class="input-field">
+                <i class="material-icons prefix">lock</i>
+                <input type="password" id="password" name="password" v-model="password" />
+                <label class="white-text" for="password">password</label>
+              </div>
+              <button v-on:click="register" class="btn btn-large lighten-4-black-text">Login</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import firebase from "firebase";
+export default {
+  name: "register",
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  methods: {
+    register: function(e) {
+      //    console.log(this.email);
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          user => {
+
+            alert(`Account created for ${user.user.email}`);
+
+            this.$router.push("/");
+          },
+          err => {
+            alert(err.message);
+          }
+        );
+      e.preventDefault();
+    }
+  }
+};
+</script>
